ce6a5b73c7ad38390d18354ba2979d493998aaa0
gonasooc <doupler@gmail.com>
2021-01-06 17:31:12 +0900

    v1.01 - slide 구현

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.main_slide').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true,
+        fade: true,
+        speed: 3000
+      });
+});
