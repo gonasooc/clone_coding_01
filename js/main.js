19ab0f3fe3cb60a835f00e1b00fef6537a542965
gonasooc <doupler@gmail.com>
2021-01-07 00:12:22 +0900

    v1.02 - 메인 안에 배너 slide 완료
    -> 배너 2개 이상 출력할 때 간격 조정 이슈 (간격 조정하는 방법 확인할 것)
    -> pause 버튼 구현 시도해볼 것

--- js/main.js
+++ js/main.js
@@ -6,5 +6,13 @@ $(function(){
         dots: true,
         fade: true,
         speed: 3000
-      });
+    });
+    $('.banner_slide').slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        speed: 1000
+    });
 });
+
