4cd22916f4ddbea6eb5b5ce019073d5f2c645a9c
gonasooc <doupler@gmail.com>
2021-01-07 23:04:00 +0900

    v1.02 - 메인 비주얼 슬라이드 완료 - 슬라이드 여러 개 설계 필요

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+    var mySwiper = new Swiper('.visual-slide', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {delay: 4000},
+        speed: 1000,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      })
+});
