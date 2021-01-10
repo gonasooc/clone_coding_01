284af867d225efc39a850a75d8237d95ae0fab15
gonasooc <doupler@gmail.com>
2021-01-11 07:44:31 +0900

    v1.03 - 슬라이드 간격은 줄 수 있는데 양끝 걸치게 하는 이슈 체크 필요

--- js/main.js
+++ js/main.js
@@ -22,4 +22,55 @@ $(function(){
           el: '.swiper-scrollbar',
         },
       })
+    var mySwiper2 = new Swiper('.best_wrap-slide', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      autoplay: {delay: 4000},
+        speed: 1000,
+      // spaceBetween: 200,
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    })
+    var mySwiper3 = new Swiper('.design_finder-slide', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 20,
+      autoplay: {delay: 4000},
+        speed: 1000,
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    })
 });
