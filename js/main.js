cc12a3ec9ae789fd131b669e923d81ddbf07411b
gonasooc <doupler@gmail.com>
2021-01-14 23:56:39 +0900

    v0.04 - media library에 btn_more 삼각 화살표 css 체크해보기

--- js/main.js
+++ js/main.js
@@ -32,8 +32,21 @@ $(document).ready(function(){
       } else {
         $('.visual-slide_wrap .btn_slide').removeClass('on');
         mySwiper.autoplay.start();
-        
         sw1 = true;
+      }
+    });
+    var sw2 = true;
+    $('#footer .footer_inner .footer_left .terms li .familysite_box button').click(function(){
+      if(sw2 == true){
+        $('#footer .footer_inner .footer_left .terms li .familysite_box button').addClass('on');
+        $('#footer .footer_inner .footer_left .terms li .familysite_box .button_list').css({'display':'block'});
+        $('#footer .footer_inner .footer_left .terms li .familysite_box button').css({'border-bottom':'none'});
+        sw2 = false;
+      } else {
+        $('#footer .footer_inner .footer_left .terms li .familysite_box button').removeClass('on');
+        $('#footer .footer_inner .footer_left .terms li .familysite_box .button_list').css({'display':'none'});
+        $('#footer .footer_inner .footer_left .terms li .familysite_box button').css({'border-bottom':'1px solid #fff'});
+        sw2 = true;
       }
     });
 });
