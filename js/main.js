2cf5b71e5313dce292e50464bd650be6535bd0cb
gonasooc <doupler@gmail.com>
2021-01-19 08:36:59 +0900

    v0.01

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.main-event-slide').slick({
+      autoplay: true,
+    });
+    $('.menu-slide').slick({
+      autoplay: true,
+      arrows: false,
+      slidesToShow: 3,
+      // dots: true
+    });
+  });
