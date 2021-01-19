6e636b08ef120b6b3a900427bd7f8dc11214114b
gonasooc <doupler@gmail.com>
2021-01-19 17:36:51 +0900

    v0.02

--- js/main.js
+++ js/main.js
@@ -3,9 +3,34 @@ $(document).ready(function(){
       autoplay: true,
     });
     $('.menu-slide').slick({
-      autoplay: true,
+      autoplay: false,
       arrows: false,
       slidesToShow: 3,
-      // dots: true
+      dots: true,
+      infinite: false
     });
+    var btn_lang_sw = true;
+    $('.lang_btn').click(function(){
+      if(btn_lang_sw == true){
+        $('.lang_list').slideDown();
+        btn_lang_sw = false;
+      } else {
+        $('.lang_list').slideUp();
+        btn_lang_sw = true;
+      }
+    });
+    $('.m_gnb .sidemenu > li > a').click(function(){
+      $(this).next().slideToggle().parent().siblings().find('.depth2').slideUp();
+    });
+    $('.m_gnb .sidemenu > li > a').click(function(){
+      $(this).toggleClass('on').parent().siblings().find('a').removeClass('on');
+    });
+
+    $('.hamburger_btn').click(function(){
+      $('#header h1 a').toggleClass('on');
+      $('.m_gnb').toggleClass('on');
+      $('.line').toggleClass('on');
+      $('body').toggleClass('on');
+    });
+
   });
