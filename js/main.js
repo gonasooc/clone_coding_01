a37c454e4d42972e12051b04e6a108567239ca76
gonasooc <doupler@gmail.com>
2021-01-20 23:26:22 +0900

    v0.04 - 2단, 3단 메뉴 구현 고민 필요 - slick slider 숫자 구현 고민 필요

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    
+    $('.main-slide').slick({
+        autoplay: true,
+        dots: true,
+        fade: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+      });
 })
