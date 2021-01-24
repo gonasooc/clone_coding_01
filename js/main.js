827888fa3088f4df8dc1a222c86a02bb9a62cd6e
gonasooc <doupler@gmail.com>
2021-01-24 14:15:17 +0900

    v0.05 - header에 검색창 구현 필요

--- js/main.js
+++ js/main.js
@@ -6,4 +6,36 @@ $(document).ready(function(){
         speed: 2000,
         autoplaySpeed: 2000,
       });
+
+    var mySwiper = new Swiper('.vitagram-slide', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 8,
+      slidesOffsetBefore: 20,
+      slidesOffsetAfter: 20,
+    
+      // If we need pagination
+      pagination: {
+        el: '.vitagram-slide .swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.vitagram-slide .swiper-button-next',
+        prevEl: '.vitagram-slide .swiper-button-prev',
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.vitagram-slide .swiper-scrollbar',
+      },
+    })
+
+    $('.login_off').mouseover(function(){
+      $('.login-box').stop().fadeIn();
+    })
+    $('.login_off').mouseleave(function(){
+      $('.login-box').stop().fadeOut();
+    })
 })
