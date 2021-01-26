3757863ab8da95ad883d46c7781f6121bc6d9e96
gonasooc <doupler@gmail.com>
2021-01-26 17:27:00 +0900

    v0.02

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
+window.onload = function() {
+    setTimeout (function() {
+    scrollTo(0, 0);
+    }, 100);
+}
+
 $(document).ready(function(){
+    $('.banner_today_wrap .btn_img_close, .banner_today_wrap input').click(function(){
+        $('.banner_today_wrap').slideUp();
+        $('#body_wrap').animate({'margin-top':0});
+    });
+
+
+    $('#header .header_inner .gnb, .gnb_fulldown').mouseenter(function(){
+        $('.gnb_fulldown').stop().slideDown(300);
+        $('.depth2').stop().slideDown(300);
+    });
+    $('#header .header_inner .gnb, .gnb_fulldown').mouseleave(function(){
+        $('.gnb_fulldown').stop().slideUp(300);
+        $('.depth2').stop().slideUp(300);
+    });
+
     
+
+
+
+
+    var footer_sw = true;
+    $('#footer button').click(function(){
+        if(footer_sw){
+            $('#footer button').removeClass('on');
+            $('#footer .family-box').removeClass('on');
+            $(this).addClass('on');
+            $(this).parent().find('.family-box').addClass('on');
+            footer_sw = false;
+        } else {
+            $(this).removeClass('on');
+            $(this).parent().find('.family-box').removeClass('on');
+            footer_sw = true;
+        }
+    });
+
+    $('#footer .close_btn').click(function(){
+        $('#footer .family-box').removeClass('on');
+        $('#footer button').removeClass('on');
+        $('#footer .family-box').removeClass('on');
+        sw = true;
+    });
+
+
+
+
 })
