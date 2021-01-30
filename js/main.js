c69f88be7ae7d27b63f6def734abcbe22aa1961c
gonasooc <doupler@gmail.com>
2021-01-31 03:25:25 +0900

    v1.05 - wdna tab-menu 구성중

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(function(){
         dots: true,
         autoplaySpeed: 3000
       });
+
+    $('#footer .btn_wrap button').mouseover(function(){
+      var tab_index_num = $(this).index();
+      console.log(tab_index_num);
+      $('#footer .btn_wrap button').removeClass('on');
+      $(this).addClass('on');
+      $('.tab-list ul').hide();
+      $('.tab-list ul').eq(tab_index_num).show();
+    });
 })
