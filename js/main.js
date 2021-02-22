8f0eccb7e5bc28ad53a2041e1a75e10f0ecc184a
gonasooc <doupler@gmail.com>
2021-02-22 17:27:07 +0900

    v0.02 - box shadow 이슈 체크 필요

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function(){
         // Optional parameters
         direction: 'horizontal',
         loop: true,
-        autoplay: {delay: 1000},
+        autoplay: {delay: 3000},
         speed: 1000,
       
         // If we need pagination
@@ -50,4 +50,33 @@ $(function(){
             sw_visual = true;
           }
       });
+
+
+      $('.exhibition-list li').mouseenter(function(){
+        $(this).addClass('on');
+        var exIndexNum = $(this).index();
+        console.log(exIndexNum);
+        $('.exhibition_bg_wrap div').eq(exIndexNum).fadeIn();
+      })
+      $('.exhibition-list li').mouseleave(function(){
+        $(this).removeClass('on');
+        $('.exhibition_bg_wrap div').fadeOut();
+      })
+     
+
 })
+
+// $('.franchise-menu a').mouseover(function(){
+//   var num = $(this).css('background-image');
+//   console.log(num);
+//   var num2 = num.replace('_off', '_on');
+//   console.log(num2);
+//   $(this).css({'background-image':num2});
+// });
+// $('.franchise-menu a').mouseleave(function(){
+//   var num = $(this).css('background-image');
+//   console.log(num);
+//   var num2 = num.replace('_on', '_off');
+//   console.log(num2);
+//   $(this).css({'background-image':num2});
+// });
